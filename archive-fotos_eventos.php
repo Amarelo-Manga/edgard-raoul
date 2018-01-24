87835d9d4e9030ae4eb165d8b81510bbbf22114b
Walace Felix <walacefex01@gmail.com>
2018-01-24 18:23:18 -0200

    arquivo alterado o botão de leia mais para a versão EN

--- archive-fotos_eventos.php
+++ archive-fotos_eventos.php
@@ -21,14 +21,22 @@
 		 	foreach( $posts as $post ){ 
 		?>
 		
-				
-	
-		
 					
 	<div class="col-lg-6 col-md-6 col-sm-12 fotos-eventos box <?php global $post; echo $post->post_name; ?> " style="background-image: url('<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>')"> 
 		<div class="efeito">
+			<?php
+				$lang = pll_current_language('slug');
+				if($lang == 'pt'){
+					 $btn = 'ver mais';
+				}else{
+					 $btn  = 'read more';
+				}
+			?>	
 			<h2 class="texto-central text-white text-center"><?php the_title() ?></h2>
-				<a href="<?php the_permalink(); ?>" class="readmore posicionando-btn margem-btn">Ver mais</a>
+				<a href="<?php the_permalink(); ?>" class="readmore posicionando-btn margem-btn"><?php echo $btn ?></a>
+
+
+
 		</div>
 				
 			</div>
